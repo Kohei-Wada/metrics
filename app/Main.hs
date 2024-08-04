66fc4c9d94ee1e0c1017eda3e87e13fef8127bee
Kohei Wada <program3152019@gmail.com>
2024-08-04 21:06:13 +0900

    update: コマンドラインから引数を指定できるように変更

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,11 @@
 module Main where
+
 import Metrics (directorySummary)
+import System.Environment (getArgs)
 
 main :: IO ()
-main = directorySummary
+main = do
+  args <- getArgs
+  case args of
+    [dir] -> directorySummary dir
+    _ -> putStrLn "Usage: app <directory>"
